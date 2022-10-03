6be259b116a93da53c2c8f580798751ec37cc84b
loud <shoorikan@gmail.com>
2022-10-03 18:01:54 +0200

    navigation play 3

---
+++ src/resourses/symbols.ts
@@ -0,0 +1,68 @@
+import { ReactComponent as FullDescription } from './FullDescription.svg';
+import { ReactComponent as RewindBackward  } from './RewindBackward.svg';
+import { ReactComponent as RewindForward   } from './RewindForward.svg';
+import { ReactComponent as ArrowDown       } from './ArrowDown.svg';
+import { ReactComponent as Settings        } from './Settings.svg';
+import { ReactComponent as ArrowUp         } from './ArrowUp.svg';
+import { ReactComponent as Pause           } from './Pause.svg';
+import { ReactComponent as Film            } from './Film.svg';
+import { ReactComponent as Exit            } from './Exit.svg';
+import { ReactComponent as Back            } from './Back.svg';
+import { ReactComponent as Fav             } from './Fav.svg';
+import { ReactComponent as FavEmpty        } from './FavEmpty.svg';
+import { ReactComponent as Tv              } from './Tv.svg';
+import { ReactComponent as TvGuide         } from './TvGuide.svg';
+import { ReactComponent as TvChannel       } from './TvChannel.svg';
+
+
+import { ReactComponent as SmallFav        } from './SmallFav.svg';
+
+import { ReactComponent as ArrowLeft       } from './ArrowLeft.svg';
+import { ReactComponent as ArrowRight      } from './ArrowRight.svg';
+import { ReactComponent as SmallArchive    } from './SmallArchive.svg';
+import { ReactComponent as Play            } from './Play.svg';
+import { ReactComponent as Search          } from './Search.svg';
+import { ReactComponent as PlayFit         } from './PlayFit.svg';
+import { ReactComponent as PauseFit        } from './PauseFit.svg';
+import { ReactComponent as Earth           } from './Earth.svg';
+
+import { ReactComponent as Backspace       } from './Backspace.svg';
+import { ReactComponent as SpaceBar        } from './SpaceBar.svg';
+import { ReactComponent as KeyLeft         } from './KeyLeft.svg';
+import { ReactComponent as KeyRight        } from './KeyRight.svg';
+import { ReactComponent as KeyEnter        } from './KeyEnter.svg';
+
+const symbols = {
+    FullDescription,
+    RewindBackward,
+    RewindForward,
+    ArrowDown,
+    Settings,
+    ArrowUp,
+    Pause,
+    Film,
+    Exit,
+    Back,
+    FavEmpty,
+    Fav,
+    Tv,
+
+    SmallFav,
+    ArrowLeft,
+    ArrowRight,
+    SmallArchive,
+    Play,
+    Search,
+    PlayFit,
+    PauseFit,
+    TvGuide,
+    TvChannel,
+    Earth,
+
+    Backspace,
+    SpaceBar,
+    KeyLeft,
+    KeyRight,
+    KeyEnter,
+};
+export default symbols;
